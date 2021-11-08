3b90bc7c9218c42c77e4ecce19935920a801f662
Shourya70073 <71991617+Shourya70073@users.noreply.github.com>
2021-11-08 10:56:21 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,21 @@
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.classList.add('myButton1')
+        status.innerHTML = "Success!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
