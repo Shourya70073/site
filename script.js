c8dea47b944ea2a60abe8f4ada6679314ca5b7f9
Shourya Srivastava <shourya70073@gmail.com>
2023-03-10 13:00:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front End Web Developer","Flutter Developer","Competitve Coder"],
+        strings: ["Front End Web Developer","Flutter Developer","Competitive Coder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
